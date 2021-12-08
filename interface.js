18daf68bcd62feff95f8fff31378af8ef4b3f7bf
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2021-12-08 19:40:04 -0300

    initial commit

---
+++ interface.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+    let squares = document.querySelectorAll(".square")
+    squares.forEach((square)=>{
+        square.addEventListener('click', handleClick)
+    })
+})
+
+function handleClick(event){
+    let square = event.target
+    let position = square.id
+    if(handleMove(position)){
+        if(playerTime == 0){
+            setTimeout(() => {alert("O jogo acabou - O venvedor foi O")}, 10)
+        }else{
+            setTimeout(() => {alert("O jogo acabou - O venvedor foi X")}, 10)
+        }
+    }
+    updateSquare(position)
+}
+
+function updateSquare(position){
+    let square = document.getElementById(position.toString())
+    let symbol = board[position]
+    square.innerHTML =`<div class='${symbol}'></div>`
+}
+
+function resetScreen(){
+    let squares = document.querySelectorAll(".square")
+    squares.forEach((square) =>{
+        square.innerHTML = ''
+    })
+}
